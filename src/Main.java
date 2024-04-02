fcecc8d3503d225cf3a6b6d477946087b3809200
Markovsky_Kot <0postaldude0@gmail.com>
2024-04-02 22:25:30 +0700

    Second version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -7,66 +5,64 @@ public class Main {
         task3();
     }
 
-    public static void checkYear(int year) {
-        String leapYear = " год является високосным";
-        String noLeapYear = " год не является високосным";
-        if (year >= 1584) {
-            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
-                System.out.println(year + leapYear);
-            } else {
-                System.out.println(year + noLeapYear);
-            }
-        } else {
-            System.out.println(year + noLeapYear);
-        }
-        //Канает?
-        //Или надо чтобы в одном методе проверялось
-        //А результат проверки передавался в другой метод, который печатает?
+    //  Задача 1
+    public static boolean checkYear(int year) {
+        return (year >= 1584) && (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static void printResultCheckYear(boolean checkYear, int year) {
+        if (checkYear) {
+            System.out.println(year + " год является високосным");
+        } else System.out.println(year + " год не является високосным");
     }
 
     public static void task1() {
-        int year = 3748;
-        checkYear(year);
+        int year = 1700;
+        boolean leapYear = checkYear(year);
+        printResultCheckYear(leapYear, year);
     }
 
-    public static void checkClientDevice(int year, int OS) {
-        if (OS == 0 && year < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (OS == 1 && year < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (OS == 0 && year >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (OS == 1 && year >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Нет информации об устройстве");
+    //  Задача 2
+    public static void checkClientDevice(int year, int typeOS) {
+        System.out.print("Установите ");
+        if (year < 2015) {
+            System.out.print("облегченную ");
         }
+        System.out.print("версию приложения для ");
+        if (typeOS == 0) {
+            System.out.println("iOS");
+        } else System.out.println("Android");
+
     }
 
     public static void task2() {
-        int currentYear = 2013; //LocalDate.now().getYear();
-        int clientTypeOS = 0;
-        checkClientDevice(currentYear, clientTypeOS);
+        int year = 2013;
+        int clientTypeOS = 1;
+        checkClientDevice(year, clientTypeOS);
     }
 
-    public static void chekDistance(int deliveryDistance) {
-        int deliveryTime;
+    //  Задача 3
+    public static int checkDistance(int deliveryDistance) {
+        int days;
         if (deliveryDistance > 0 && deliveryDistance <= 20) {
-            deliveryTime = 1;
-            System.out.println("Потребуется дней: " + deliveryTime);
+            days = 1;
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            deliveryTime = 2;
-            System.out.println("Потребуется дней: " + deliveryTime);
+            days = 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            deliveryTime = 3;
-            System.out.println("Потребуется дней: " + deliveryTime);
+            days = 3;
         } else {
-            System.out.println("Доставки нет");
+            days = 0;
         }
+        return days;
     }
-
+public static void printDeliveryTime(int days){
+    if (days > 0) {
+        System.out.println("Потребуется дней: " + days);
+    } else System.out.println("Доставки нет");
+}
     public static void task3() {
-        int deliveryDistance = 57;
-        chekDistance(deliveryDistance);
+        int deliveryDistance = 70;
+        int deliveryTime = checkDistance(deliveryDistance);
+        printDeliveryTime(deliveryTime);
     }
 }
